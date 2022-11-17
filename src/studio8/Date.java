057f6e3b372a43077de4fc4218cfb5aa4587ae94
kevingithub2021 <83477419+kevingithub2021@users.noreply.github.com>
2022-11-17 17:08:16 -0600

    Tried it!

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,85 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
+	
+	private int month;
+	private int day;
+	private int year;
+	private boolean isHoliday;
+	
+public Date (int month, int day, int year, boolean isHoliday) {
+	this.month=month;
+	this.year=year;
+	this.day=day;
+	this.isHoliday=isHoliday;
+	
+}
+
+    public int getMonth() {
+	return month;
+}
+
+public void setMonth(int month) {
+	this.month = month;
+}
+
+public int getDay() {
+	return day;
+}
+
+public void setDay(int day) {
+	this.day = day;
+}
+
+public int getYear() {
+	return year;
+}
+
+public void setYear(int year) {
+	this.year = year;
+}
+
+public boolean isHoliday() {
+	return isHoliday;
+}
+
+public void setHoliday(boolean isHoliday) {
+	this.isHoliday = isHoliday;
+}
+
+public String toString() {
+	return getMonth()+" "+getDay()+" "+getYear()+" ";
+}
 
 
-    public static void main(String[] args) {
+
+	@Override
+public int hashCode() {
+	return Objects.hash(day, month, year);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Date other = (Date) obj;
+	return day == other.day && month == other.month && year == other.year;
+}
+
+	public static void main(String[] args) {
+		
+		Date thisDate=new Date(12, 17, 2022, false);
+		System.out.print(thisDate);
+		
+		Date thatDate = new Date(12, 15, 2022, false);
+		
+		System.out.print(thatDate.equals(thisDate));
 
     }
 
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,95 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	private int minutes;
+	private int hours;
+	private boolean isTwoFour;
+	
+	public Time (int minutes, int hours, boolean isTwoFour) {
+		this.minutes=minutes;
+		this.hours=hours;
+		this.isTwoFour=isTwoFour;
+		
+	}
+	
+		
+
+	public int getMinutes() {
+		return minutes;
+	}
+
+
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+
+
+	public int getHours() {
+		
+		return this.hours;
+	}
+
+
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+
+
+	public boolean isTwoFour() {
+		return isTwoFour;
+	}
+
+
+
+	public void setTwoFour(boolean isTwoFour) {
+		this.isTwoFour = isTwoFour;
+	}
+
+	public String toString() {
+		return getMinutes()+" "+getHours()+" ";
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(minutes, hours);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this.hashCode() == obj.hashCode())
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hours == other.hours && minutes == other.minutes;
+	}
+
+
 
 	public static void main(String[] args) {
- 
-    	
+		
+		Time thisTime= new Time (45, 13, false);
+		
+		System.out.print(thisTime);
+		
+		Time thatTime = new Time (45, 13, true);
+		
+		
+		
+		System.out.print(thatTime.equals(thisTime));
+			
     }
 
 }
